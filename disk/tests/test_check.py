d9a2961713bb10fa53dab2217f850af1804f4134
Bogdan Berezovyi <bberezov@cisco.com>
2018-08-21 11:32:20 +0300

    Added service_check_rw and tests

--- disk/tests/test_check.py
+++ disk/tests/test_check.py
@@ -49,10 +49,11 @@ RATES_VALUES = {
 
 
 class MockPart(object):
-    def __init__(self, device=DEFAULT_DEVICE_NAME, fstype='ext4', mountpoint='/'):
+    def __init__(self, device=DEFAULT_DEVICE_NAME, fstype='ext4', mountpoint='/', opts='rw'):
         self.device = device
         self.fstype = fstype
         self.mountpoint = mountpoint
+        self.opts = opts
 
 
 class MockDiskMetrics(object):
@@ -121,6 +122,7 @@ def test_default_options():
     assert check._all_partitions is False
     assert check._excluded_disk_re == re.compile('^$')
     assert check._device_tag_re == []
+    assert check._service_check_rw is False
 
 def test_disk_check(aggregator):
     """
@@ -207,6 +209,16 @@ def test_psutil(aggregator, psutil_mocks):
             aggregator.assert_metric(name, value=value, tags=['device:{}'.format(DEFAULT_DEVICE_NAME)])
 
     assert aggregator.metrics_asserted_pct == 100.0
+
+def test_psutil_rw(aggregator, psutil_mocks):
+    """
+    Mock psutil and run the check
+    """
+    instances = [{'service_check_rw': True}]
+    c = Disk('disk', None, {}, instances)
+    c.check(instances[0])
+
+    aggregator.assert_service_check('disk.read_write', status=0)
 
 def test_use_mount(aggregator, psutil_mocks):
     """
